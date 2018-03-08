19b7e2197a57ee2559a3e33234730bedfac2db82
CoderHF <54666674@qq.com>
2018-03-08 16:15:08 +0800

    Initial Commit

---
+++ ChooseLanguage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChooseLanguage
+//
+//  Created by CoderHF on 2018/3/8.
+//  Copyright © 2018年 CoderHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
